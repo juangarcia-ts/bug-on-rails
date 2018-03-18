e20abaa6514dad745f0a2dd78e1ef3e2cf5ef913
juangarcia-ts <juangarcia.ts@gmail.com>
2018-03-18 12:37:07 -0700

    Correção da criação de contas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :username, :password, :password_confirmation) }
+  end
+
   protected
   def authenticate_user!(options={})
     if user_signed_in?
